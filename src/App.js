27b173c43dcd36f17751e7e1cbe9d9ab8ed4cadc
LauraMontalvo <laura.montalvo2406@gmail.com>
2022-09-10 23:18:23 -0500

    mi comit2

--- src/App.js
+++ src/App.js
@@ -8,16 +8,10 @@ import Ventas from './Views/Ventas';
 import ListaFacturas from './Views/ListaFacturas';
 import DetalleFactura from './Views/DetalleFactura';
 import RealizarVenta from './Views/RealizarVenta';
-<<<<<<< HEAD
 import RegistrarProducto from './Views/RegistroProducto';
 import EditarProducto from './Views/EditarProducto';
 
 import ListaProductos from './Views/ListaProductos';
-=======
-import Proveedores from './Views/Proveedores';
-import ProveedorIngresar from './Views/ProveedorIngresar';
-import ProveedorUpdate from './Views/ProveedorUpdate';
->>>>>>> 191140664bb73f989bf000ada46147e5e9abdbfe
 
 function App() {
   return (
@@ -30,20 +24,13 @@ function App() {
             <Route path="/ventas" element={<Ventas/>} />
             <Route path='/historial' element={<ListaFacturas/>}/>
             <Route exact path='/detalleFactura/:id' element={<DetalleFactura/>} />
-<<<<<<< HEAD
 
 
             <Route exact path="/productos" element={<RegistrarProducto />}/>
       
             <Route  path='/listaproductos' element={<ListaProductos/>} />
             <Route exact path="/listaproductos/:id/edit" element = {<EditarProducto/>}/>
-            ss
-=======
-            <Route path="/vender" element={<RealizarVenta/>}/>
-            <Route exact path="/proveedor" element={<Proveedores/>}/>
-            <Route exact path="/proveedor/new" element={<ProveedorIngresar/>}/>
-            <Route exact path="/edit/:id" element={<ProveedorUpdate/>} />
->>>>>>> 191140664bb73f989bf000ada46147e5e9abdbfe
+            
           </Routes>
         </BrowserRouter>
     </div>
